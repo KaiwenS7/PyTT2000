ab36f57f0a1ec4c8d88e4e6622dadad6a953883e
Kevin Steele <kevsteele7@gmail.com>
2025-11-15 02:22:42 +0000

    adding reverse operation

--- tests/test_values.py
+++ tests/test_values.py
@@ -1,11 +1,15 @@
 import pytest
 
-from PyTT2000.tt2k import unixTott2k
+from PyTT2000.tt2k import unixTott2k, tt2kToUnix
 
 
 def test_answer():
     assert unixTott2k(1760042158,0) == 813314227184000000
     assert unixTott2k(1760042158,99) == 813314227184099000
+
+def test_tt2k():
+    assert tt2kToUnix(813314227184000000) == [1760042158,0]
+    assert tt2kToUnix(813314227184099000) == [1760042158,99]
 
 def test_fail():
     with pytest.raises(ValueError) as excinfo:
